1c6d47a4ef16b51a93dddd0914651574ae3c18d0
Furqan <furqan.ali8093@gmail.com>
2024-12-23 01:43:44 -0500

    Task 2

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,26 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => total + item.cost.slice(1) * item.quantity, 0).toFixed(2);
   };
-
-  const handleContinueShopping = (e) => {
-   
-  };
-
-
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1){
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return (parseFloat(item.cost.slice(1)) * item.quantity).toFixed(2);
   };
 
   return (
