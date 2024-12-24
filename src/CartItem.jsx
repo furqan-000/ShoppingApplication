b273a9b66aad1e6357f111d0ef20969ddf169a0a
Furqan <furqan.ali8093@gmail.com>
2024-12-24 02:14:39 -0500

    Task 4

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -3,13 +3,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
-const CartItem = ({ onContinueShopping }) => {
-  const cart = useSelector(state => state.cart.items);
+const CartItem = ({ items, onRemoveItem, onUpdateQuantity, onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-    return cart.reduce((total, item) => total + item.cost.slice(1) * item.quantity, 0).toFixed(2);
+    return items
+    .reduce((total, item) => total + parseFloat(item.cost.slice(1)) * item.quantity, 0)
+    .toFixed(2);
   };
 
   const handleIncrement = (item) => {
