ebf93815399c2ca1b85278595f167ae99196dd26
Furqan <furqan.ali8093@gmail.com>
2024-12-24 03:19:53 -0500

    Coding error

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,14 +7,14 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-        const {name, image, cost} = action.payload;
+        const { name, image, cost } = action.payload;
         const existingItem = state.items.find(item => item.name === name);
 
         if (existingItem) {
             existingItem.quantity ++;
         
         } else {
-            state.items.push({name, image, cost, quantity: 1 });
+            state.items.push({ name, image, cost, quantity: 1 });
         }
     },
     removeItem: (state, action) => {
@@ -22,7 +22,7 @@ export const CartSlice = createSlice({
         state.items = state.items.filter(item => item.name !== nameToRemove);
     },
     updateQuantity: (state, action) => {
-        const {name, quantity} = action.payload;
+        const { name, quantity } = action.payload;
         const itemToUpdate = state.items.find(item => item.name === name);
 
         if (itemToUpdate) {
